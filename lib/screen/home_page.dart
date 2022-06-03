22d7b170d614b6be01e6566144017c65a7d8b822
allos <sadeosun@axxess.com>
2022-06-03 13:22:07 +0100

    message chat created

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,9 +1,19 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:ichat_app/allConstants/color_constants.dart';
+import 'package:ichat_app/Utilities/debouncer.dart';
+import 'package:ichat_app/Utilities/utilities.dart';
+import 'package:ichat_app/allConstants/constants.dart';
 import 'package:ichat_app/allModel/popup_choices.dart';
+import 'package:ichat_app/allModel/user_chat.dart';
 import 'package:ichat_app/allProvider/auth_provider.dart';
+import 'package:ichat_app/allProvider/home_provider.dart';
+import 'package:ichat_app/allWidgets/loading_view.dart';
 import 'package:ichat_app/main.dart';
+import 'package:ichat_app/screen/chat_page.dart';
 import 'package:ichat_app/screen/login_page.dart';
 import 'package:ichat_app/screen/setting_page.dart';
 import 'package:provider/provider.dart';
@@ -18,15 +28,19 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final GoogleSignIn googleSignIn = GoogleSignIn();
   final ScrollController listScrollController = ScrollController();
+  Debouncer searchDebouncer = Debouncer(milliseconds: 300);
+  StreamController<bool> btnClearController = StreamController<bool>();
+  TextEditingController searchBarController = TextEditingController();
 
   late String currentUserId;
   late AuthProvider authProvider;
+  late HomeProvider homeProvider;
 
   @override
   void initState() {
     super.initState();
     authProvider = context.read<AuthProvider>();
-    //homeProvider = context.read<HomeProvider>();
+    homeProvider = context.read<HomeProvider>();
 
     if (authProvider.getUserFirebaseId()!.isNotEmpty) {
       currentUserId = authProvider.getUserFirebaseId()!;
@@ -36,6 +50,13 @@ class _HomePageState extends State<HomePage> {
           (Route<dynamic> route) => false);
     }
     listScrollController.addListener(scrollListener);
+    //searchBarController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    btnClearController.close();
   }
 
   void scrollListener() {
@@ -80,6 +101,110 @@ class _HomePageState extends State<HomePage> {
           builder: (context) => const SettingPage(),
         ),
       );
+    }
+  }
+
+  Future<bool> onbackPress() {
+    openDialog();
+    return Future.value(false);
+  }
+
+  Future<void> openDialog() async {
+    switch (await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            clipBehavior: Clip.hardEdge,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(0),
+            ),
+            contentPadding: EdgeInsets.zero,
+            children: [
+              Container(
+                color: ColorConstants.themeColor,
+                padding: const EdgeInsets.only(bottom: 10, top: 10),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      child: const Icon(
+                        Icons.exit_to_app,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                      margin: const EdgeInsets.only(bottom: 10),
+                    ),
+                    const Text(
+                      'Exit app',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Text(
+                      'Are you sure to exit app?',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.white70,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, 0);
+                },
+                child: Row(
+                  children: [
+                    Container(
+                      child: Icon(
+                        Icons.cancel,
+                        color: ColorConstants.primaryColor,
+                      ),
+                      margin: EdgeInsets.only(right: 10),
+                    ),
+                    Text(
+                      'Cancel',
+                      style: TextStyle(
+                        color: ColorConstants.primaryColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, 1);
+                },
+                child: Row(
+                  children: [
+                    Container(
+                      child: const Icon(
+                        Icons.check_circle,
+                        color: ColorConstants.primaryColor,
+                      ),
+                      margin: const EdgeInsets.only(right: 10),
+                    ),
+                    const Text(
+                      'Yes',
+                      style: TextStyle(
+                        color: ColorConstants.primaryColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          );
+        })) {
+      case 0:
+        break;
+      case 1:
+        exit(0);
     }
   }
 
@@ -136,6 +261,249 @@ class _HomePageState extends State<HomePage> {
           )
         ],
       ),
+      body: WillPopScope(
+        onWillPop: onbackPress,
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                buildSearchBar(),
+                Expanded(
+                  child: StreamBuilder<QuerySnapshot>(
+                    stream: homeProvider.getStreamFireStore(
+                      FirestoreConstants.pathUserCollection,
+                      _limit,
+                      _textSearch,
+                    ),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        if ((snapshot.data?.docs.length ?? 0) > 0) {
+                          return ListView.builder(
+                            padding: const EdgeInsets.all(10),
+                            itemCount: snapshot.data?.docs.length,
+                            itemBuilder: (context, index) =>
+                                buildItem(context, snapshot.data?.docs[index]),
+                            controller: listScrollController,
+                          );
+                        } else {
+                          return const Center(
+                            child: Text(
+                              'No user found...',
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          );
+                        }
+                      } else {
+                        return const Center(
+                          child: CircularProgressIndicator(
+                            color: Colors.grey,
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+              child: isLoading ? const LoadingView() : const SizedBox.shrink(),
+            )
+          ],
+        ),
+      ),
     );
+  }
+
+  Widget buildSearchBar() {
+    return Container(
+      height: 40,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Icon(
+            Icons.search,
+            color: ColorConstants.greyColor,
+            size: 20,
+          ),
+          const SizedBox(width: 5),
+          Expanded(
+            child: TextFormField(
+              textInputAction: TextInputAction.search,
+              controller: searchBarController,
+              onChanged: (value) {
+                if (value.isNotEmpty) {
+                  btnClearController.add(true);
+                  setState(() {
+                    _textSearch = value;
+                  });
+                } else {
+                  btnClearController.add(false);
+                  setState(() {
+                    _textSearch = "";
+                  });
+                }
+              },
+              decoration: const InputDecoration.collapsed(
+                hintText: 'Search here...',
+                hintStyle: TextStyle(
+                  fontSize: 13,
+                  color: ColorConstants.greyColor,
+                ),
+              ),
+              style: const TextStyle(fontSize: 13),
+            ),
+          ),
+          StreamBuilder(
+            stream: btnClearController.stream,
+            builder: (context, snapshot) {
+              return snapshot.data == true
+                  ? GestureDetector(
+                      onTap: () {
+                        searchBarController.clear();
+                        btnClearController.add(false);
+                        setState(() {
+                          _textSearch = "";
+                        });
+                      },
+                      child: const Icon(
+                        Icons.clear_rounded,
+                        color: ColorConstants.greyColor,
+                        size: 20,
+                      ),
+                    )
+                  : const SizedBox.shrink();
+            },
+          )
+        ],
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: ColorConstants.greyColor2,
+      ),
+      padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+      margin: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+    );
+  }
+
+  Widget buildItem(BuildContext context, DocumentSnapshot? document) {
+    if (document != null) {
+      UserChat userChat = UserChat.fromDocument(document);
+      if (userChat.id == currentUserId) {
+        return const SizedBox.shrink();
+      } else {
+        return Container(
+          child: TextButton(
+            onPressed: () {
+              if (Utilities.isKeyboardShowing()) {
+                Utilities.closeKeyboard(context);
+              }
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ChatPage(
+                    peerId: userChat.id,
+                    peerAvatar: userChat.photoUrl,
+                    peerNickname: userChat.nickname,
+                  ),
+                ),
+              );
+            },
+            style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(Colors.grey.withOpacity(.2)),
+                shape: MaterialStateProperty.all(
+                  const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                )),
+            child: Row(
+              children: [
+                Material(
+                  child: userChat.photoUrl.isNotEmpty
+                      ? Image.network(
+                          userChat.photoUrl,
+                          fit: BoxFit.cover,
+                          width: 50,
+                          height: 50,
+                          loadingBuilder: (BuildContext context, Widget child,
+                              ImageChunkEvent? loadingProgress) {
+                            if (loadingProgress == null) return child;
+                            return Container(
+                              width: 50,
+                              height: 50,
+                              child: CircularProgressIndicator(
+                                color: Colors.grey,
+                                value: loadingProgress.expectedTotalBytes !=
+                                            null &&
+                                        loadingProgress.expectedTotalBytes !=
+                                            null
+                                    ? loadingProgress.cumulativeBytesLoaded /
+                                        loadingProgress.expectedTotalBytes!
+                                    : null,
+                              ),
+                            );
+                          },
+                          errorBuilder: (context, object, stackTrace) {
+                            return const Icon(
+                              Icons.account_circle,
+                              size: 50,
+                              color: ColorConstants.greyColor,
+                            );
+                          },
+                        )
+                      : const Icon(
+                          Icons.account_circle,
+                          size: 50,
+                          color: ColorConstants.greyColor,
+                        ),
+                  borderRadius: const BorderRadius.all(Radius.circular(25)),
+                  clipBehavior: Clip.hardEdge,
+                ),
+                Flexible(
+                  child: Container(
+                    child: Column(
+                      children: [
+                        Container(
+                          child: Text(
+                            userChat.nickname,
+                            maxLines: 1,
+                            style: TextStyle(
+                              color: Colors.grey[600],
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          alignment: Alignment.centerLeft,
+                          margin: const EdgeInsets.fromLTRB(10, 0, 0, 5),
+                        ),
+                        Container(
+                          child: Text(
+                            userChat.aboutMe,
+                            maxLines: 1,
+                            style: TextStyle(
+                              color: Colors.grey[700],
+                            ),
+                          ),
+                          alignment: Alignment.centerLeft,
+                          margin: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                        ),
+                      ],
+                    ),
+                    margin: const EdgeInsets.only(left: 20),
+                  ),
+                )
+              ],
+            ),
+          ),
+          margin: const EdgeInsets.only(bottom: 10, left: 5, right: 5),
+        );
+      }
+    } else {
+      return const SizedBox.shrink();
+    }
   }
 }
